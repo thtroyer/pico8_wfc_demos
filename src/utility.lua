3ecf6a58e6fad45310249bd4b826b5c41902a62d
Tom Troyer <tom.troyer@gmail.com>
2023-01-14 16:42:45 -0500

    Cleanup

--- src/utility.lua
+++ src/utility.lua
@@ -1,30 +1,34 @@
 
--- converts anything to string, even nested tables
-function tostring(any) 
-	if type(any) == "function" then
+-- utility methods
+function tostring(var) 
+	if var == nil then
+		return "nil"
+	end
+
+	if type(var) == "function" then
 		return "function"
 	end
-	if any == nil then
-				return "nil"
+
+	if type(var) == "string" then
+		return var
 	end
-	if type(any) == "string" then
-		return any
-	end
-	if type(any) == "boolean" then
-		if any then
+
+	if type(var) == "boolean" then
+		if var then
 			return "true"
 		end
 		return "false"
 	end
-	if type(any) == "table" then
-		local str = "{ "
-		for k, v in pairs(any) do
-			str = str .. tostring(k) .. "->" .. tostring(v) .. " "
+
+	if type(var) == "table" then
+		local table = "{ "
+		for k, v in pairs(var) do
+			table = table .. tostring(k) .. "->" .. tostring(v) .. " "
 		end
-		return str .. "}"
+		return table .. "}"
 	end
-	if type(any) == "number" then
-		return "" .. any
+	if type(var) == "number" then
+		return "" .. var
 	end
 	return "unknown"
 end
@@ -34,7 +38,7 @@ function random(minimum, maximum)
 end
 
 function random_int(low, high)
-return flr(rnd(high + 1 - low)) + low
+	return flr(rnd(high + 1 - low)) + low
 end
 
 function log(msg)
